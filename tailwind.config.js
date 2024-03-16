fb2a1105a7b2e4478461747dc224a8ef7689626f
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2024-03-16 11:11:25 -0700

    Font attempt 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,18 @@ module.exports = {
     fontFamily: {
       sans: ['Syne', 'sans-serif'],
     },
+    fontWeight: {
+      thin: '100',
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
