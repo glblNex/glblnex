99adb04adaaed8525fc8bf8819711ccf81ab5bd3
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2024-03-16 10:52:42 -0700

    Font & SEO

--- app/(routes)/layout.js
+++ app/(routes)/layout.js
@@ -8,13 +8,16 @@ import Loading from './loading';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import CTABanner from '../_components/CTABanner';
 
+
 export const metadata = {
   title: {
     template: '%s | globalNex',
-    default: 'globalNex',
+    default: 'globalNex - Global Trade Data Intelligence',
   },
-  description: '...',
-  keywords: ['Next.js', 'React', 'JavaScript'],
+  description: 'GlobalNex is a cutting-edge data analytics platform that revolutionizes the way organizations access, analyze, and leverage global trade data. GlobalNex provides a centralized and intelligent solution for stakeholders across the international trade ecosystem, enabling them to gain unparalleled insights, optimize operations, and drive strategic decision-making.',
+  keywords: ['Global Trade', 'Data', 'AI', 'Global AI', 'Global Trade Data', 'Analytics', 'Global Analytics', 'Data Intelligence', 'Global Intelligence', 'Logistics', 'Advanced Logistics', 'Global Tracking', ' AI Logistics', 'AI Global Trade', 'Trade Data', 'Logistical Data', 'Logistics Tracking'],
+  type: 'website',
+  url: 'https://glblnex.com/',
   icons: {
     icon: 'icons/favicon.ico',
     shortcut: 'icons/favicon.ico',
@@ -24,7 +27,11 @@ export const metadata = {
       url: 'icons/apple-touch-icon.png',
     },
   },
+  canonical: 'https://glblnex.com/',
   manifest: 'icons/site.webmanifest',
+  preconnect: 'https://fonts.googleapis.com',
+  preconnect: { url: 'https://fonts.gstatic.com', crossorigin },
+  stylesheet: 'https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap',
 }
 
 export default function Layout({ children }) {
@@ -34,7 +41,7 @@ export default function Layout({ children }) {
         <MainNavbar />
         <Suspense fallback={<Loading />}>
           <div className='container mx-auto min-w-full z-1'>
-              <main>{children}</main>
+            <main>{children}</main>
           </div>
         </Suspense>
         {/* <CTABanner/> */}
