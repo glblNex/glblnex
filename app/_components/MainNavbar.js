d15689a5341300d05e8ac2e968da2a06067f1b7e
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2024-05-03 14:34:58 -0700

    Improved Design Aspects

--- app/_components/MainNavbar.js
+++ app/_components/MainNavbar.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image'
 import MainButton from './MainButton'
 import Hamburger from "./Hamburger";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function MainNavbar() {
   const [burgerOn, setBurger] = useState(false);
@@ -14,9 +14,26 @@ export default function MainNavbar() {
     setBurger(!burgerOn);
   }
 
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    // Attach the event listener
+    window.addEventListener("scroll", handleScroll);
+    // Remove the event listener on cleanup
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav className="container mx-auto min-w-full z-40">
-      <div className="fixed top-0 left-0 right-0 px-10 lg:px-24 py-6 z-40">
+      <div className={`fixed top-0 left-0 right-0 px-10 lg:px-24 py-3 z-40 transition-all duration-500 ${isScrolled ? "backdrop-blur-xl backdrop-brightness-75 shadow-l shadow-black" : ""}`}>
         {/* Desktop */}
         <div className='hidden lg:block '>
           <div className='grid grid-cols-3 gap-x-3.5 justify-stretch content-center'>
